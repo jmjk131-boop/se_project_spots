2a4426b4e7336ecb4e918a8fa1fbd2e242e8e744
jordan-michael kirk <jmjk131@gmail.com>
2025-12-16 17:07:16 -0500

    Fixed spelling mistake

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const intitialCards = [
+const initialCards = [
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
@@ -55,7 +55,7 @@ newPostCloseButton.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
 
-intitialCards.forEach(function (card) {
+initialCards.forEach(function (card) {
   console.log(card.name);
   console.log(card.link);
 });
